0b3e51da35632941c58ff16ea184a5c640e38ff8
PiotrChoinski <97666907+PiotrChoinski@users.noreply.github.com>
2024-12-02 22:44:45 +0100

    little form

--- bmi_mobile/lib/pages/add_user_page.dart
+++ bmi_mobile/lib/pages/add_user_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class AddUserPage extends StatefulWidget {
   const AddUserPage({super.key});
@@ -10,7 +12,25 @@ class AddUserPage extends StatefulWidget {
 class _AddUserPageState extends State<AddUserPage> {
   @override
   Widget build(BuildContext context) {
-    return const CupertinoPageScaffold(
-        child: Center(child: Text('Add User Page')));
+    return CupertinoPageScaffold(
+        navigationBar: const CupertinoNavigationBar(
+          middle: Text('Add User'),
+          backgroundColor: Colors.transparent,
+          previousPageTitle: 'Users List',
+        ),
+        child: SafeArea(
+            child: Form(
+          autovalidateMode: AutovalidateMode.always,
+          child: CupertinoFormSection.insetGrouped(
+              header: const Text('Add new user'),
+              children: [
+                CupertinoTextFormFieldRow(
+                  placeholder: 'Name',
+                ),
+                CupertinoTextFormFieldRow(
+                  placeholder: 'Surname',
+                ),
+              ]),
+        )));
   }
 }
