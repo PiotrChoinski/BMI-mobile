683fac8b3e6beaf59e0b22beda1b52f011efd6ae
PiotrChoinski <97666907+PiotrChoinski@users.noreply.github.com>
2024-12-02 21:49:40 +0100

    list of users

---
+++ bmi_mobile/android/app/src/main/kotlin/com/example/bmi_mobile/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.bmi_mobile
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ bmi_mobile/lib/pages/users_list_page.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class UsersListPage extends StatefulWidget {
+  const UsersListPage({super.key});
+
+  @override
+  State<UsersListPage> createState() => _UsersListPageState();
+}
+
+class Item {
+  final String prefix;
+  final String helper;
+  const Item({required this.prefix, required this.helper});
+}
+
+List<Item> items = [
+  const Item(prefix: "Italiano", helper: 'Italiano'),
+  const Item(prefix: "English", helper: 'English'),
+  const Item(prefix: "Español", helper: 'Español'),
+  const Item(prefix: "Français", helper: 'Français'),
+  const Item(prefix: "Polish", helper: 'Polish'),
+];
+
+class _UsersListPageState extends State<UsersListPage> {
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+        navigationBar: const CupertinoNavigationBar(
+          middle: Text('Users List'),
+          backgroundColor: Colors.transparent,
+        ),
+        child: ListView.builder(
+            itemCount: items.length,
+            itemBuilder: (context, index) => Dismissible(
+                key: Key(index.toString()),
+                onDismissed: (direction) {
+                  print('Dismissed');
+                },
+                background: Container(color: Colors.red),
+                child: CupertinoListTile(
+                  onTap: () {
+                    showCupertinoDialog(
+                        context: context,
+                        builder: (context) => CupertinoAlertDialog(
+                              title: Text(items[index].prefix),
+                              content: Text(items[index].helper),
+                              actions: [
+                                CupertinoDialogAction(
+                                    child: const Text('OK'),
+                                    onPressed: () {
+                                      Navigator.of(context).pop();
+                                    }),
+                              ],
+                            ));
+                  },
+                  leading: const Icon(CupertinoIcons.person),
+                  title: Text(items[index].prefix),
+                  trailing: const Icon(CupertinoIcons.forward),
+                ))));
+  }
+}
